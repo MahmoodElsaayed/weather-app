6db5dcdbf51f6ccda74864a275a255819c8b7d4d
MahmoudElsayed <mahmood.m.elsaayed@gmail.com>
2025-04-04 20:34:12 +0200

    refactor: Wrap module in a factory function for DI

--- src/js/api.js
+++ src/js/api.js
@@ -1,36 +1,40 @@
 import { convertDatetimeToHumanDate } from './util.js'
 
-export async function fetchResponse(url) {
-    try {
-        const response = await fetch(url)
-        if (!response.ok) {
-            const error = new Error(`Error ${response.status}`)
-            error.statusCode = response.status
-            throw error
+export default function API() {
+    async function fetchResponse(url) {
+        try {
+            const response = await fetch(url)
+            if (!response.ok) {
+                const error = new Error(`Error ${response.status}`)
+                error.statusCode = response.status
+                throw error
+            }
+            return {
+                statusCode: response.status,
+                data: await response.json(),
+            }
+        } catch (error) {
+            return error
         }
+    }
+
+    function filterWeatherResponse(response) {
         return {
-            statusCode: response.status,
-            data: await response.json(),
+            address: response.data.resolvedAddress,
+            forecast: response.data.days.map((day) => ({
+                conditions: day.conditions,
+                date: convertDatetimeToHumanDate(day.datetime),
+                humidity: day.humidity,
+                wind: day.windspeed,
+                precipitation: day.precip,
+                temperature: day.temp,
+            })),
         }
-    } catch (error) {
-        return error
     }
-}
 
-export function filterWeatherResponse(response) {
-    return {
-        address: response.data.resolvedAddress,
-        forecast: response.data.days.map((day) => ({
-            conditions: day.conditions,
-            date: convertDatetimeToHumanDate(day.datetime),
-            humidity: day.humidity,
-            wind: day.windspeed,
-            precipitation: day.precip,
-            temperature: day.temp,
-        })),
+    function filterImageResponse(response) {
+        return { src: response.data.results[0].urls.full }
     }
-}
 
-export function filterImageResponse(response) {
-    return { src: response.data.results[0].urls.full }
+    return { fetchResponse, filterWeatherResponse, filterImageResponse }
 }
