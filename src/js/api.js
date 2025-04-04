0bde3f1c7c2408f4471a3b94e3db23af51d7de45
MahmoudElsayed <mahmood.m.elsaayed@gmail.com>
2025-04-04 18:44:24 +0200

    refactor: Modularize weather.js into a reusable API module
    - Decoubled fetching logic in fetchWeather from the visualCrossing API
    and refactored it into a general fetchResponse function.

--- src/js/api.js
+++ src/js/api.js
@@ -1,8 +1,6 @@
 import { convertDatetimeToHumanDate } from './util.js'
 
-async function fetchWeather(location, unit = 'metric') {
-    const key = 'FXNJH5S5MNRED8WVD9PU8CDNZ' // It's a free one dumbass!
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=${unit}&include=days&key=${key}&contentType=json`
+export async function fetchResponse(url) {
     try {
         const response = await fetch(url)
         if (!response.ok) {
@@ -19,10 +17,10 @@ async function fetchWeather(location, unit = 'metric') {
     }
 }
 
-function filterWeatherResponse(response) {
+export function filterWeatherResponse(response) {
     return {
-        address: response.resolvedAddress,
-        forecast: response.days.map((day) => ({
+        address: response.data.resolvedAddress,
+        forecast: response.data.days.map((day) => ({
             conditions: day.conditions,
             date: convertDatetimeToHumanDate(day.datetime),
             humidity: day.humidity,
@@ -32,5 +30,3 @@ function filterWeatherResponse(response) {
         })),
     }
 }
-
-export { fetchWeather, filterWeatherResponse }
